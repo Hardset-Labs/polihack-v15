fdffe0fa7664230029619a6950c24ab5f6290ad5
Vlad <durdeuvladioan@gmail.com>
2024-04-12 20:24:32 +0300

    finished preparations

---
+++ polihackv15/polihack_site/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),  # Default home URL
+    path("home/", views.home, name="home")
+]# Default home URL
--- polihackv15/polihack_site/views.py
+++ polihackv15/polihack_site/views.py
@@ -1,3 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'polihack_site/home.html')
+
+
+def add_subject(request):
+    return render(request, 'polihack_site/add_subject.html')
+
+
+def create_learning_plan(request):
+    return JsonResponse({'status': 'success'})
