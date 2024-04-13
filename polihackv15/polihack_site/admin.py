476d50de35621d12f86b8b35df4af95fb53f295a
Vlad <durdeuvladioan@gmail.com>
2024-04-13 21:42:25 +0300

    task done

--- polihackv15/polihack_site/admin.py
+++ polihackv15/polihack_site/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import UserData, Subject, LearningMinutesDay, Chapter
+
+admin.site.register(UserData)
+admin.site.register(Subject)
+admin.site.register(LearningMinutesDay)
+admin.site.register(Chapter)
+
--- polihackv15/polihack_site/urls.py
+++ polihackv15/polihack_site/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),  # Default home URL
     path("home/", views.home, name="home"),
-    path("subjects/", views.subjects, name="subjects"),
+    path("subjects/", views.get_subjects, name="subjects"),
     path("study_plan/", views.study_plan, name="study_plan"),
-    path('add_subject/', views.add_subject, name='add_subject')
+    path('add_subject/', views.add_subject, name='add_subject'),
+    path('edit_subject/<int:item_id>/', views.edit_subject, name='edit_subject')
 ]# Default home URL
