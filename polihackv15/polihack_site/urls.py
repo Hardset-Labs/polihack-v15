7cff480e922d0fdcd44391879595d4c1e78a9c57
Strungar Dan <110939116+Danutz2oo3@users.noreply.github.com>
2024-04-13 13:27:54 +0300

    setup made for subject

--- polihackv15/polihack_site/urls.py
+++ polihackv15/polihack_site/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path    ("", views.home, name="home"),  # Default home URL
-    path("home/", views.home, name="home")
+    path("", views.home, name="home"),  # Default home URL
+    path("home/", views.home, name="home"),
+    path("subjects/", views.subjects, name="subjects"),
 ]# Default home URL
