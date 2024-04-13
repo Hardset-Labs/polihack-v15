fd88c4d74bb086ba39af90b7fc065c7936e4a06d
Strungar Dan <110939116+Danutz2oo3@users.noreply.github.com>
2024-04-13 12:42:00 +0300

    homepage done

--- polihackv15/polihack_site/urls.py
+++ polihackv15/polihack_site/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),  # Default home URL
+    path    ("", views.home, name="home"),  # Default home URL
     path("home/", views.home, name="home")
 ]# Default home URL
