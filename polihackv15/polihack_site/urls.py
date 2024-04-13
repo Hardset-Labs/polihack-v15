8533615ca16337883605d04edc7d84f3487e854a
Strungar Dan <110939116+Danutz2oo3@users.noreply.github.com>
2024-04-13 19:02:45 +0300

    added study_plan

--- polihackv15/polihack_site/urls.py
+++ polihackv15/polihack_site/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),  # Default home URL
     path("home/", views.home, name="home"),
     path("subjects/", views.subjects, name="subjects"),
+    path("study_plan/", views.study_plan, name="study_plan"),
 ]# Default home URL
