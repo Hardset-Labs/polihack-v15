9b786274d1678fcf3c7457f362d2869a5e64c7e0
Strungar Dan <110939116+Danutz2oo3@users.noreply.github.com>
2024-04-14 13:25:56 +0300

    all should be good now

--- polihackv15/polihack_site/urls.py
+++ polihackv15/polihack_site/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('edit_subject/<int:item_id>/', views.edit_subject, name='edit_subject'),
     path('learn_now/', views.learn_now, name='learn_now'),
     path('learn_subject/<int:subject_id>/', views.learn_subject, name='learn_subject'),
-    path('delete_subject/<int:subject_id>/', views.delete_subject, name='delete_subject')
+    path('delete_subject/<int:subject_id>/', views.delete_subject, name='delete_subject'),
+    path('loading/', views.loading_page, name='loading_page')
 ]# Default home URL
